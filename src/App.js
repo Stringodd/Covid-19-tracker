9de47de8e5f7c13ac8048a7f6830b4675906cb45
Eray Eroglu <eray_6960@hotmail.com>
2022-07-15 17:21:44 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [veri, setVeri] = useState();
+  const [tarih, setTarih] = useState();
+
+  useEffect(() => {
+    axios.get("https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json")
+      .then(json => setVeri(json.data[tarih]))
+      .catch(err => console.log(err))
+
+  }, [veri, tarih])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment className='container'>
+      <React.Fragment className='row'>
+        <div className='col-md-8 mx-auto mt-4'>
+          <h2 className='text-center text-white display-4'>TÜRKİYE COVID 19 ARAMA MOTORU</h2>
+          <input type="text" placeholder='GG/AA/YYYY' className='form-control'
+            onChange={(event) => setTarih(event.target.value)} />
+          <table class="table">
+            <thead className='text-white'>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Hasta Sayısı</th>
+                <th scope="col">Test Sayısı</th>
+                <th scope="col">Ölü Sayısı</th>
+                <th scope="col">İyileşen Sayısı</th>
+              </tr>
+            </thead>
+            <tbody className='text-white'>
+              <tr className={veri === undefined ? "bg-danger" : "bg-success"}>
+                <th scope="row">Günlük</th>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.patients}</td>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.tests}</td>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.deaths}</td>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.recovered}</td>
+              </tr>
+              <tr className={veri === undefined ? "bg-danger" : "bg-success"}>
+                <th scope="row">Toplam</th>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.totalPatients}</td>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.totalTests}</td>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.totalDeaths}</td>
+                <td>{veri === undefined ? "Veri bekleniyor" : veri.totalRecovered}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </React.Fragment>
+    </React.Fragment>
   );
 }
 
